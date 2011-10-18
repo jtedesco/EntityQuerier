de8d54703a646b3a30967f847fd65f566c41be75
Jon Tedesco <jon@jontedesco.net>
2011-10-18 16:30:17 -0500

    Added and tested ability to parse search google results from a list of URLs

---
+++ util/GoogleResultsBuilder.py
@@ -0,0 +1,32 @@
+from src.search.google.GoogleResultParserThread import GoogleResultParserThread
+
+__author__ = 'jon'
+
+def buildResultsFromURLs(urls, pageRankScriptPath):
+    """
+      Builds a list of results in the same format as those retrieved from a search engine, using a list of URLs to populate
+        the results.
+    """
+
+    # Build the formatted list of results
+    results = []
+    for url in urls:
+        results.append({
+            'url' : url
+        })
+
+    # Create threads to process the pages for this set of results
+    threads = []
+    for resultData in results:
+        parserThread = GoogleResultParserThread(resultData, pageRankScriptPath)
+        threads.append(parserThread)
+
+    # Launch all threads
+    for thread in threads:
+        thread.start()
+
+    # Wait for all the threads to finish
+    for thread in threads:
+        thread.join()
+
+    return results
