5a654776143bdf7c9cfa7fc9a547adc0e0c49f3f
Jon Tedesco <jon@jontedesco.net>
2011-10-18 16:40:50 -0500

    Updated docs

---
+++ src/evaluation/NaiveQueryEvaluation.py
@@ -0,0 +1,18 @@
+from src.evaluation.QueryEvaluation import QueryEvaluation
+
+__author__ = 'jon'
+
+class QueryEvaluation(QueryEvaluation):
+    """
+      Represents the high-level interface used to evaluate the completeness of results from a query.
+    """
+
+    def evaluate(self, results, idealResults):
+        """
+          Score the effectiveness of this query, given the results retrieved and the ideal results
+
+            @param  results         The results retrieved from the search engine, in the format retrieved
+            @param  idealResults    The results that would be ideal, in the same format
+        """
+
+        return -1
