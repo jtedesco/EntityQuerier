7b2be03c89e5ad8b9577feadf7e3188e38254f9d
Jon Tedesco <jon@jontedesco.net>
2011-11-09 23:32:01 -0600

    Added DMOZ crawling

---
+++ src/dmoz/DMOZCrawler.py
@@ -0,0 +1,178 @@
+from BeautifulSoup import BeautifulSoup
+import re
+from src.dmoz.DMOZCrawlerThread import DMOZCrawlerThread
+
+__author__ = 'jon'
+
+class DMOZCrawler(object):
+    """
+      Crawler class that encapsulates the crawler logic. This file is intended to be run once, to fetch as much data from DMOZ as possible.
+    """
+
+    def __init__(self):
+
+        # The root DMOZ urls from which to start
+        self.urlsToCrawl = [
+            'http://www.dmoz.org/Shopping/',
+            'http://www.dmoz.org/Reference/',
+            'http://www.dmoz.org/Kids_and_Teens/',
+            'http://www.dmoz.org/Games/',
+            'http://www.dmoz.org/Arts/',
+            'http://www.dmoz.org/Society/',
+            'http://www.dmoz.org/Regional/',
+            'http://www.dmoz.org/News/',
+            'http://www.dmoz.org/Health/',
+            'http://www.dmoz.org/Business/',
+            'http://www.dmoz.org/Sports/',
+            'http://www.dmoz.org/Science/',
+            'http://www.dmoz.org/Recreation/',
+            'http://www.dmoz.org/Home/',
+            'http://www.dmoz.org/Computers/'
+        ]
+
+        self.urlsCrawled = set([])
+
+
+    def __getLinksFromPage(self, content):
+
+        # Parse content
+        soup = BeautifulSoup(content)
+        referenceLists = soup.findAll('ul', {'class' : 'directory dir-col'})
+        links = []
+        for referenceList in referenceLists:
+            linkElements = referenceList.findAll('a')
+            for link in linkElements:
+                relativeUrl = link.attrs[0][1]
+                absoluteUrl = self.__buildAbsoluteLinks([relativeUrl])
+                links.extend(absoluteUrl)
+
+        return links
+
+
+    def __getUrlsFromPage(self, content):
+
+        # Parse content
+        soup = BeautifulSoup(content)
+        referenceLists = soup.findAll('ul', {'class' : 'directory-url'})
+        urls = []
+        for referenceList in referenceLists:
+            linkElements = referenceList.findAll('a')
+            for link in linkElements:
+                urls.append(link.attrs[0][1])
+                
+        return urls
+
+
+    def __launchThreads(self, crawlData, saveData = False):
+
+        # Create threads to fetch these pages
+        threads = []
+        for url in crawlData:
+            parserThread = DMOZCrawlerThread(crawlData[url], saveData)
+            threads.append(parserThread)
+
+        # Launch all threads
+        for thread in threads:
+            thread.start()
+
+        # Wait for all the threads to finish
+        for thread in threads:
+            # Allow up to 5 seconds for this thread to respond, otherwise, kill it
+            thread.join(5)
+
+            # Kill it if it hung
+            if thread.isAlive():
+                try:
+                    thread._Thread__stop()
+                except:
+                    print(str(thread.getName()) + ' could not be terminated')
+
+
+    def __buildAbsoluteLinks(self, links):
+        """
+          Builds a list of absolute links for a given list of relative links and a parent/root url.
+        """
+
+        # Create a new list of absolute links
+        absoluteLinks = []
+
+        # Rewrite all of the given links to absolute links
+        for link in links:
+
+            # Rewrite relative-absolute links
+            if link.startswith('/'):
+                link = 'http://www.dmoz.org' + link
+                if link not in absoluteLinks:
+                    absoluteLinks.append(link)
+
+            # Take care of absolute links
+            elif link not in absoluteLinks and link.startswith('http'):
+                absoluteLinks.append(link)
+
+            # Rewrite relative links
+            elif not link.startswith('http'):
+                link = parentUrl + '/' + link
+                if link not in absoluteLinks:
+                    absoluteLinks.append(link)
+
+        return absoluteLinks
+
+
+    def run(self):
+
+        while True:
+
+            # Grab the URLs to crawl in this phase
+            urlsToCrawl = self.urlsToCrawl
+            self.urlsToCrawl = []
+
+            # Create some data structure to hold the crawl data
+            crawlData = {}
+            for url in urlsToCrawl:
+                crawlData[url] = {
+                    'url' : url
+                }
+
+            # Launch threads to fetch all of this data in parallel
+            self.__launchThreads(crawlData)
+
+            # Examine crawled data from this stage, and extract pages to crawl
+            urlsFound = set([])
+            for url in crawlData:
+
+                # Get the relevant links from this page (to other pages to crawl)
+                content = crawlData[url]['content']
+                linksFromPage = self.__getLinksFromPage(content)
+
+                # Add all links from this page that haven't been crawled
+                for linkFromPage in linksFromPage:
+                    if linkFromPage not in self.urlsCrawled:
+                        self.urlsToCrawl.append(linkFromPage)
+
+                # Get all web links from this page
+                urlsFromPage = self.__getUrlsFromPage(content)
+                for url in urlsFromPage:
+                    if url not in self.urlsCrawled:
+                        urlsFound.add(url)
+
+
+            # Crawl all actual links from the web
+            webData = {}
+            for url in urlsFound:
+                webData[url] = {
+                    'url' : url
+                }
+            self.__launchThreads(webData, True)
+
+
+            # Record the links we've already crawled
+            for url in crawlData:
+                self.urlsCrawled.add(url)
+
+
+
+if __name__ == '__main__':
+    
+    crawler = DMOZCrawler()
+    crawler.run()
+
---
+++ src/dmoz/DMOZCrawlerThread.py
@@ -0,0 +1,89 @@
+import hashlib
+import os
+from pprint import pformat
+import threading
+from urllib2 import HTTPError, URLError
+import sys
+from src.search.SearchResultParsing import isHTML, getPageContent, loadFromUrl, parseMetaDataFromContent
+from util.PRCache import PRCache
+
+__author__ = 'jon'
+
+class DMOZCrawlerThread(threading.Thread):
+
+    
+    def __init__(self, resultDictionary, saveData):
+
+        threading.Thread.__init__(self)
+
+        self.resultDictionary = resultDictionary
+        self.url = resultDictionary['url']
+
+        # Find where we expect this data to be cached
+        self.savePath = str(os.getcwd())
+        self.savePath = self.savePath[:self.savePath.find('EntityQuerier') + len('EntityQuerier')] + '/dmoz/'
+
+        # Whether or not we should be saving the data to disk
+        self.saveData = saveData
+
+        # A cache for computing PR
+        self.prCache = PRCache()
+
+
+        
+    def run(self):
+        """
+          Parse the content of this page, and update the given dictionary for this thread
+        """
+
+        try:
+
+            # Get the content from this page
+            print "Getting page content for '%s'" % self.url.strip()
+
+            filename = self.__encodeCacheFilename(self.url)
+
+            try:
+                content = loadFromUrl(self.url)
+            except ValueError:
+                content = None
+                print "Error with URL: " + self.url
+
+            # Extract the content from this page
+            if content is not None and isHTML(content):
+
+                # Get the information about this url
+                content = content.lower()
+                if self.saveData:
+                    title, keywords, description = parseMetaDataFromContent(content)
+                    pageRank = self.prCache.getPageRank(self.url)
+
+                self.resultDictionary['content'] = content
+
+                
+                if self.saveData:
+
+                    # Store the extra data
+                    self.resultDictionary['keywords'] = keywords
+                    self.resultDictionary['description'] = description
+                    self.resultDictionary['pageRank'] = pageRank
+                    self.resultDictionary['title'] = title
+
+                    # Save the result file
+                    open(filename, 'w').write(pformat(self.resultDictionary))
+
+
+        except URLError:
+            print("Error accessing '%s', %s" % (self.url.strip(), str(sys.exc_info()[1]).strip()))
+
+
+    def __encodeCacheFilename(self, url):
+        """
+          Encode the URL to a filename to be stored in the cache
+        """
+        try:
+            hashedUrl = hashlib.sha256(url).hexdigest()
+        except UnicodeDecodeError:
+            hashedUrl = hashlib.sha256(url.decode(errors='ignore')).hexdigest()
+        filename = self.savePath + hashedUrl
+        return filename
