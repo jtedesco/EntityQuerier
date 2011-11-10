1cefb27a0c27f6fcf2d93c2db181bfb4f96137e8
Jon Tedesco <jon@jontedesco.net>
2011-11-09 18:21:42 -0600

    Updated parameters to query

--- src/search/Search.py
+++ src/search/Search.py
@@ -15,13 +15,12 @@ class Search(object):
         self.numberOfResultsToRetrieve = numberOfResultsToRetrieve
 
         
-    def query(self, query, fetchContent=True, lastQuery=False):
+    def query(self, query, fetchContent=True):
         """
           Query the search interface and return a dictionary of results
 
             @param  query           The query to search
             @param  fetchContent    Whether or not to retrieve the content of pages as well as just summaries + urls from google
-            @param  lastQuery       Whether or not this query is the last that will be submitted for the search process
         """
         
         raise NotImplementedError("Must instantiate a concrete SearchInterface!")
