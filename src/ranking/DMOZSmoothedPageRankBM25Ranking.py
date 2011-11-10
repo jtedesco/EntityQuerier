5fe3fd63d62635404079fa1b11b92e6c25c16788
Jon Tedesco <jon@jontedesco.net>
2011-11-10 03:22:34 -0600

    Implemented input file cleaning for reading dumped DMOZ results

--- src/ranking/DMOZSmoothedPageRankBM25Ranking.py
+++ src/ranking/DMOZSmoothedPageRankBM25Ranking.py
@@ -1,5 +1,6 @@
-from json import load
+from json import load, loads
 import os
+import re
 from src.ranking.PageRankBM25Ranking import PageRankBM25Ranking
 
 __author__ = 'jon'
@@ -19,7 +20,29 @@ class DMOZSmoothedPageRankBM25Ranking(PageRankBM25Ranking):
         # Supplement the index with the DMOZ documents
         dmozResults = []
         for filename in os.listdir(self.dmozPath):
-            dmozResult = load(open(self.dmozPath + filename))
+
+            # Get the contents of the file
+            dmozResultData = open(self.dmozPath + filename).read()
+
+            # Parse it out
+            dmozResultData = dmozResultData.replace("\"", "\\\"")
+            dmozResultData = dmozResultData.replace("'", "\"")
+            dmozResultData = dmozResultData.replace("\n", " ")
+
+            # Remove extra whitespace
+            removeExtraWhitespaceRegex = re.compile(r'\s+')
+            dmozResultData = re.sub(removeExtraWhitespaceRegex, ' ', dmozResultData)
+
+            # Remove 'set' keywords
+            dmozResultData = dmozResultData.replace("set([", "[")
+            dmozResultData = dmozResultData.replace("])", "]")
+
+            # Remove 'u' for unicode data
+            dmozResultData = dmozResultData.replace(" u'", " '")
+            dmozResultData = dmozResultData.replace(" u\"", " \"")
+
+
+            dmozResult = loads(dmozResultData)
             dmozResults.append(dmozResult)
 
         # Create the index with both the traditional and new DMOZ search results
