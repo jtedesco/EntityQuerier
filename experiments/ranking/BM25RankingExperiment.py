f03211fd619a2d374aa4959148b7d627e048a120
Jon Tedesco <jon@jontedesco.net>
2011-11-14 02:31:50 -0600

    Fixed input file paths

--- experiments/ranking/BM25RankingExperiment.py
+++ experiments/ranking/BM25RankingExperiment.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     entity = load(open(projectRoot + '/entities/Kevin Chen-Chuan Chang.json'))
 
     # Rank the results
-    retrievalResults = '/experiments/retrieval/results/KevinChang-EntityAttributeNames'
+    retrievalResults = '/experiments/retrieval/results/KevinChang-EntityAttributeNamesAndValues'
     rankingExperiment = RankingExperiment(projectRoot + retrievalResults, entity, BM25Ranking, False, True)
     results = rankingExperiment.rank()
 
