20c6c137404cb71b5ca16386ea4ee3fdc8096ff8
Jon Tedesco <jon@jontedesco.net>
2011-11-15 15:41:25 -0600

    Added comma separation for entries

--- src/ranking/TermFrequencyRanking.py
+++ src/ranking/TermFrequencyRanking.py
@@ -4,7 +4,7 @@ from pprint import pprint
 import whoosh
 from whoosh.analysis import StemmingAnalyzer, CharsetFilter
 from whoosh.fields import Schema, TEXT, ID, NUMERIC, KEYWORD
-from whoosh.index import create_in
+from whoosh.index import create_in, exists_in, open_dir
 from whoosh.qparser.default import QueryParser
 from whoosh.qparser.plugins import PlusMinusPlugin
 from whoosh.qparser.syntax import OrGroup
@@ -61,76 +61,82 @@ class TermFrequencyRanking(TermVectorRanking):
         indexDirectory = self.getIndexLocation()
 
         # Remove the index if it exists
-        if os.path.exists(indexDirectory):
-            os.removedirs(indexDirectory)
+        if not os.path.exists(indexDirectory):
 
-        # Try to create the index directory
-        os.mkdir(indexDirectory)
+            # Try to create the index directory
+            os.mkdir(indexDirectory)
 
-        # Build a new index in this directory
-        self.index = create_in(indexDirectory, self.indexSchema)
+            # Build a new index in this directory
+            self.index = create_in(indexDirectory, self.indexSchema)
 
-        # Get a writer for the index
-        indexWriter = self.index.writer()
+            # Get a writer for the index
+            indexWriter = self.index.writer()
 
-        # Walk the pages folder for content
-        for searchResult in self.searchResults:
+            # Walk the pages folder for content
+            for searchResult in self.searchResults:
 
-            try:
                 try:
-                    unicodeContent = unicode(searchResult['content'], errors='ignore')
-                except TypeError:
-                    unicodeContent = searchResult['content']
-                try:
-                    unicodeTitle = unicode(searchResult['title'], errors='ignore')
-                except TypeError:
-                    unicodeTitle = searchResult['title']
-                try:
-                    unicodeDescription = unicode(searchResult['description'], errors='ignore')
-                except TypeError:
-                    unicodeDescription = searchResult['description']
-                try:
-                    unicodeUrl = unicode(searchResult['url'], errors='ignore')
-                except TypeError:
-                    unicodeUrl = searchResult['url']
-                try:
-                    unicodeKeywords = unicode(' '.join(searchResult['keywords']), errors='ignore')
-                except TypeError:
-                    unicodeKeywords = ', '.join(searchResult['keywords'])
-                try:
-                    unicodeYqlKeywords = unicode(' '.join(searchResult['yqlKeywords']), errors='ignore')
-                except TypeError:
-                    unicodeYqlKeywords = ', '.join(searchResult['yqlKeywords'])
-                try:
-                    unicodeHeaders = unicode(' '.join(searchResult['headers']), errors='ignore')
-                except TypeError:
-                    unicodeHeaders = ', '.join(searchResult['headers'])
+                    try:
+                        unicodeContent = unicode(searchResult['content'], errors='ignore')
+                    except TypeError:
+                        unicodeContent = searchResult['content']
+                    try:
+                        unicodeTitle = unicode(searchResult['title'], errors='ignore')
+                    except TypeError:
+                        unicodeTitle = searchResult['title']
+                    try:
+                        unicodeDescription = unicode(searchResult['description'], errors='ignore')
+                    except TypeError:
+                        unicodeDescription = searchResult['description']
+                    try:
+                        unicodeUrl = unicode(searchResult['url'], errors='ignore')
+                    except TypeError:
+                        unicodeUrl = searchResult['url']
+                    try:
+                        unicodeKeywords = unicode(', '.join(searchResult['keywords']), errors='ignore')
+                    except TypeError:
+                        unicodeKeywords = ', '.join(searchResult['keywords'])
+                    try:
+                        unicodeYqlKeywords = unicode(', '.join(searchResult['yqlKeywords']), errors='ignore')
+                    except TypeError:
+                        unicodeYqlKeywords = ', '.join(searchResult['yqlKeywords'])
+                    try:
+                        unicodeHeaders = unicode(', '.join(searchResult['headers']), errors='ignore')
+                    except TypeError:
+                        unicodeHeaders = ', '.join(searchResult['headers'])
 
-                pageRank = searchResult['pageRank']
+                    pageRank = searchResult['pageRank']
 
-                if len(unicodeContent) == 0:
-                    unicodeContent = u'?'
-                if len(unicodeTitle) == 0:
-                    unicodeTitle = u'?'
-                if len(unicodeDescription) == 0:
-                    unicodeDescription = u'?'
-                if len(unicodeUrl) == 0:
-                    unicodeUrl = u'?'
-                if len(unicodeKeywords) == 0:
-                    unicodeKeywords = u'?'
-                if len(unicodeYqlKeywords) == 0:
-                    unicodeYqlKeywords = u'?'
-                if len(unicodeHeaders) == 0:
-                    unicodeHeaders = u'?'
+                    if len(unicodeContent) == 0:
+                        unicodeContent = u'?'
+                    if len(unicodeTitle) == 0:
+                        unicodeTitle = u'?'
+                    if len(unicodeDescription) == 0:
+                        unicodeDescription = u'?'
+                    if len(unicodeUrl) == 0:
+                        unicodeUrl = u'?'
+                    if len(unicodeKeywords) == 0:
+                        unicodeKeywords = u'?'
+                    if len(unicodeYqlKeywords) == 0:
+                        unicodeYqlKeywords = u'?'
+                    if len(unicodeHeaders) == 0:
+                        unicodeHeaders = u'?'
 
-                indexWriter.add_document(content=unicodeContent, title=unicodeTitle, description=unicodeDescription, yqlKeywords=unicodeYqlKeywords,
-                                     pagerank=pageRank, url=unicodeUrl, keywords=unicodeKeywords, headers=unicodeHeaders)
+                    indexWriter.add_document(content=unicodeContent, title=unicodeTitle, description=unicodeDescription, yqlKeywords=unicodeYqlKeywords,
+                                         pagerank=pageRank, url=unicodeUrl, keywords=unicodeKeywords, headers=unicodeHeaders)
 
-            except KeyError:
-                pass
+                except KeyError:
+                    pass
 
-        # Commit all the changes, so that every change is flushed to disk, and we can safely query the index
-        indexWriter.commit()
+            # Commit all the changes, so that every change is flushed to disk, and we can safely query the index
+            indexWriter.commit()
+
+        else:
+            if exists_in(indexDirectory):
+                print "Opening index..."
+                self.index = open_dir(indexDirectory)
+            else:
+                raise Exception("Could not open index directory!")
 
 
     def queryIndex(self, weightingMechanism):
