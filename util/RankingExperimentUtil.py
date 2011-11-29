5832baafa4511c6400cfed6a71884181537f8bd3
Jon Tedesco <jon@jontedesco.net>
2011-11-28 22:53:37 -0600

    Added r-precision and precision at 100% recall point

--- util/RankingExperimentUtil.py
+++ util/RankingExperimentUtil.py
@@ -15,15 +15,16 @@ def outputRankingResults(entityId, outputFile, outputTitle, projectRoot, results
     # Find the 'standard' for this entity -- the true list of relevant results
     relevantUrls = load(open(projectRoot + '/standard/' + entityId + '.json'))
 
-    recallAt10, recallAt20, recallAt50, recallAt100, precisionAt10, precisionAt20, precisionAt50, precisionAt100, \
-        averagePrecisionAt10, averagePrecisionAt20, averagePrecisionAt50, averagePrecisionAt100 = getRankingResults(results, relevantUrls, cutoff)
+    recallAt1, recallAt10, recallAt20, recallAt50, precisionAt1, precisionAt10, precisionAt20, precisionAt50, \
+        averagePrecisionAt1, averagePrecisionAt10, averagePrecisionAt20, averagePrecisionAt50, rPrecision, fullPrecision = getRankingResults(results, relevantUrls, cutoff)
 
     # Build the output
     output = outputTitle % cutoff
-    output += "Precision @ 10:  %1.5f | Recall @ 10:  %1.5f | Average Precision @ 10:  %1.5f\n" % (precisionAt10, recallAt10, averagePrecisionAt10)
-    output += "Precision @ 20:  %1.5f | Recall @ 20:  %1.5f | Average Precision @ 20:  %1.5f\n" % (precisionAt20, recallAt20, averagePrecisionAt20)
-    output += "Precision @ 50:  %1.5f | Recall @ 50:  %1.5f | Average Precision @ 50:  %1.5f\n" % (precisionAt50, recallAt50, averagePrecisionAt50)
-    output += "Precision @ 100: %1.5f | Recall @ 100: %1.5f | Average Precision @ 100:  %1.5f\n\n" % (precisionAt100, recallAt100, averagePrecisionAt100)
+    output += "Precision @ 1:  %1.5f | Average Precision @ 1:  %1.5f\n" % (precisionAt1, averagePrecisionAt1)
+    output += "Precision @ 10:  %1.5f | Average Precision @ 10:  %1.5f\n" % (precisionAt10, averagePrecisionAt10)
+    output += "Precision @ 20:  %1.5f | Average Precision @ 20:  %1.5f\n" % (precisionAt20, averagePrecisionAt20)
+    output += "Precision @ 50: %1.5f | Average Precision @ 50:  %1.5f\n\n" % (precisionAt50, averagePrecisionAt50)
+    output += "Recall @ 50: %1.5f | R-Precision:  %1.5f | Precision at 100 Percent (full) Recall Point: %1.5f\n\n" % (recallAt50, rPrecision, fullPrecision)
     output += "Score         Url\n"
     output += "=====         ===\n"
     count = 0
@@ -53,18 +54,20 @@ def getRankingResults(results, relevantUrls, cutoff):
     # Prepare vars & data structs
     relevantUrlsFound = []
     count = 0
+    precisionAt1 = 0
     precisionAt10 = 0
     precisionAt20 = 0
     precisionAt50 = 0
-    precisionAt100 = 0
+    recallAt1 = 0
     recallAt10 = 0
     recallAt20 = 0
     recallAt50 = 0
-    recallAt100 = 0
+    averagePrecisionAt1 = 0
     averagePrecisionAt10 = 0
     averagePrecisionAt20 = 0
     averagePrecisionAt50 = 0
-    averagePrecisionAt100 = 0
+    rPrecision = 0
+
 
     if len(results) > 0 and type(results[0]) == type({}):
         includesScores = False
@@ -72,6 +75,7 @@ def getRankingResults(results, relevantUrls, cutoff):
         includesScores = True
 
     averagePrecisionRunningTotal = 0.0
+    fullPrecision = 0
     for result in results:
         count += 1
         if count > cutoff:
@@ -88,13 +92,23 @@ def getRankingResults(results, relevantUrls, cutoff):
                 wasRelevant = True
                 relevantUrlsFound.append(result['url'])
 
+        # Precision at 100% recall point
+        if wasRelevant:
+            fullPrecision = float(len(relevantUrlsFound)) / count
 
         # Update running total for average precision
         if wasRelevant:
             averagePrecisionRunningTotal += float(len(relevantUrlsFound)) / count
 
         # Gather precision & recall data
-        if count == 10:
+        if count == 1:
+            precisionAt1 = float(len(relevantUrlsFound))
+            recallAt1 = float(len(relevantUrlsFound)) / len(relevantUrls)
+            try:
+                averagePrecisionAt1 = averagePrecisionRunningTotal / len(relevantUrlsFound)
+            except ZeroDivisionError:
+                averagePrecisionAt1 = 0
+        elif count == 10:
             precisionAt10 = float(len(relevantUrlsFound)) / 10
             recallAt10 = float(len(relevantUrlsFound)) / len(relevantUrls)
             try:
@@ -115,13 +129,10 @@ def getRankingResults(results, relevantUrls, cutoff):
                 averagePrecisionAt50 = averagePrecisionRunningTotal / len(relevantUrlsFound)
             except ZeroDivisionError:
                 averagePrecisionAt50 = 0
-        elif count == 100:
-            precisionAt100 = float(len(relevantUrlsFound)) / 100
-            recallAt100 = float(len(relevantUrlsFound)) / len(relevantUrls)
-            try:
-                averagePrecisionAt100 = averagePrecisionRunningTotal / len(relevantUrlsFound)
-            except ZeroDivisionError:
-                averagePrecisionAt100 = 0
 
-    return recallAt10, recallAt20, recallAt50, recallAt100, precisionAt10, precisionAt20, precisionAt50,\
-           precisionAt100, averagePrecisionAt10, averagePrecisionAt20, averagePrecisionAt50, averagePrecisionAt100
+        # r-precision
+        if count == len(relevantUrls):
+            rPrecision = float(len(relevantUrlsFound)) / count
+
+    return recallAt1, recallAt10, recallAt20, recallAt50, precisionAt1, precisionAt10, precisionAt20, precisionAt50,\
+           averagePrecisionAt1, averagePrecisionAt10, averagePrecisionAt20, averagePrecisionAt50, rPrecision, fullPrecision
